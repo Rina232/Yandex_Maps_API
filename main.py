b24a0dee90e2b8de6a9d3a81d53905c2ab5274d2
Rin232 <ekaterina>
2023-02-04 10:46:22 +0300

    .

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+import pygame
+import requests
+
+api_server = "http://static-maps.yandex.ru/1.x/"
+
+ZOOM = 10
+LON, LAT = "23.734862", "37.975487"
+params = {
+    "ll": ",".join([LON, LAT]),
+    "z": ZOOM,
+    "l": "map"
+}
+response = requests.get(api_server, params=params)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(response.url)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+pygame.display.set_caption('Большая задача по Maps API')
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # screen.fill((0, 0, 0))
+    # pygame.display.flip()
+pygame.quit()
+
+os.remove(map_file)
