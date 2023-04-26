cb082a8428d5718c1093c8a7c8ec7162efcf3602
ekate <ekaterina_kostrykina2006@gmail.com>
2023-04-26 17:39:58 +0300

    .

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 api_server = "http://static-maps.yandex.ru/1.x/"
 
-ZOOM = 12
+ZOOM = 5
 LON, LAT = "23.734862", "37.975487"
 
 params = {
@@ -34,9 +34,24 @@ def file_update(ZOOM, LON, LAT):
 
     with open(map_file, "wb") as file:
         file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
 
 
-file_update(ZOOM, LON, LAT)
+params = {
+        "ll": ",".join([LON, LAT]),
+        "z": ZOOM,
+        "l": "map"
+    }
+response = requests.get(api_server, params=params)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(response.url)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+with open(map_file, "wb") as file:
+    file.write(response.content)
 
 
 pygame.init()
@@ -50,15 +65,38 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
+            print(LAT)
             if event.key == pygame.K_PAGEUP:
                 if ZOOM < 23:
                     ZOOM += 1
                     file_update(ZOOM, LON, LAT)
-            if event.key == pygame.K_PAGEDOWN:
+            elif event.key == pygame.K_PAGEDOWN:
                 if ZOOM > 0:
                     ZOOM -= 1
                     file_update(ZOOM, LON, LAT)
-    screen.blit(pygame.image.load(map_file), (0, 0))
+            elif event.key == pygame.K_UP:
+
+                if float(LAT) + 5 <= 87:
+                    LAT = str(float(LAT) + 5)
+                    file_update(ZOOM, LON, LAT)
+            elif event.key == pygame.K_DOWN:
+                if float(LAT) - 5 >= -87:
+                    LAT = str(float(LAT) - 5)
+                    file_update(ZOOM, LON, LAT)
+
+            elif event.key == pygame.K_RIGHT:
+                if float(LON) + 5 <= 180:
+                    LON = str(float(LON) + 5)
+                    file_update(ZOOM, LON, LAT)
+                else:
+                    LON = str(-((float(LON) + 5) % 180) - 180)
+            elif event.key == pygame.K_LEFT:
+                if float(LON) - 5 >= -180:
+                    LON = str(float(LON) - 5)
+                    file_update(ZOOM, LON, LAT)
+                else:
+                    LON = str(180 - (float(LON) - 5 + 180))
+
     pygame.display.flip()
 
 
